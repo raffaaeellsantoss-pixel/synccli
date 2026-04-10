0fb851892fb3be968e062deff28ac607c5072518
Rafael Santos <raffaaeell.santoss@gmail.com>
2026-04-10 12:34:55 -0300

    feat: initial project setup with Spring Boot and WebSocket

---
+++ src/main/java/com/synccli/SynccliApplication.java
@@ -0,0 +1,13 @@
+package com.synccli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynccliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynccliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/synccli/SynccliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synccli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynccliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
